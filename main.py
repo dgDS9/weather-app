59232adcfd523896b90e9b6f0b6e284684e63f3e
dgDS9 <daniela.gaer@gmail.com>
2026-03-05 11:27:33 +0100

    ucommited rows

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        # "http://localhost:5500",
-        # "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
         "https://dgds9.github.io",
     ],
     allow_credentials=False,   # <— wichtig
